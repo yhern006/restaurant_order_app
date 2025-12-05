0cb1dcfc2204ca33175842fe030b8323d485f9a3
Yvette Hernandez <yhern006@users.noreply.github.com>
2025-12-04 17:41:33 -0800

    added plus icon for orders

--- index.js
+++ index.js
@@ -7,19 +7,26 @@ function setup_menu(){
         const item_html = `
             <li class='menu_item'>
                 <div class='item_container'>
-                    <div class='item_emoji'>
-                        ${item.emoji}
+                    <div class='container_start'>
+                        <div class='item_emoji'>
+                            ${item.emoji}
+                        </div>
+                        <div class='item_info'>
+                            <p class='item_name'>
+                                ${item.name}
+                            </p>
+                            <p class='item_ingredients'>
+                                ${item.ingredients}
+                            </p>
+                            <p class='item_price'>
+                                $${item.price}
+                            </p>
+                        </div>
                     </div>
-                    <div class='item_info'>
-                        <p class='item_name'>
-                            ${item.name}
-                        </p>
-                        <p class='item_ingredients'>
-                            ${item.ingredients}
-                        </p>
-                        <p class='item_price'>
-                            $${item.price}
-                        </p>
+                    <div class='container_end'>
+                        <span class="material-symbols-outlined md-40">
+                        add_circle
+                        </span>
                     </div>
                 </div>
             </li>
