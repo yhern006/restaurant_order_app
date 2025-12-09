31ede5fbffa9e1594454ee6f3935ce3fa8a72dce
Yvette Hernandez <yhern006@users.noreply.github.com>
2025-12-08 21:23:32 -0800

    added order summary

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import { menuArray } from "./data.js";
 
-const menu_list = document.getElementById('menu_ul')
+const menuListEl = document.getElementById('menu_ul')
+const customerOrderEl = document.getElementById('customer_order')
+let orderArray = [{
+    item: "Pizza",
+    quantity: 2,
+    totalPrice: 28
+},{
+    item: "Beer",
+    quantify: 1,
+    totalPrice: 12
+}]
 
-function setup_menu(){
+function setup_menu() {
     menuArray.forEach(function(item){
         const item_html = `
             <li class='menu_item'>
@@ -31,8 +41,40 @@ function setup_menu(){
                 </div>
             </li>
         `
-        menu_list.innerHTML += item_html
+        menuListEl.innerHTML += item_html
     })
 }
 
+function renderCustomerOrder() {
+    const checkoutCartEl = document.getElementById('checkout_cart')
+    const totalOrderPriceEl = document.getElementById('total_order_price')
+    
+    let order_html = orderArray.map(function(item){
+        return `
+            <li class='cart_item'>
+                <div class='cart_start'>
+                    ${item.item}
+                    <button>remove</button>
+                </div>
+                $${item.totalPrice}
+            </li>
+        `
+    }).join('')
+
+    checkoutCartEl.innerHTML = order_html
+    customerOrderEl.style.display = 'block'
+    totalOrderPriceEl.textContent = '$' + getTotalOrderPrice()
+}
+
+function getTotalOrderPrice() {
+    return orderArray.reduce((total, currentItem) => {
+        return total + currentItem.totalPrice
+    }, 0)
+}
+
+function addOrderItem() {
+
+}
+
 setup_menu()
+renderCustomerOrder()
